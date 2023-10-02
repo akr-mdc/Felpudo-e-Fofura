0c02f8d653128ca9f9f07eb41bfd618152d33750
akr-mdc <caio.machado11235@gmail.com>
2023-10-02 01:40:01 -0300

    First Commit

---
+++ Assets/Assets_Lab06/Assets_Lab06/MoveFelpudo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFelpudo : MonoBehaviour
+{
+	public float speed = 5.0f;
+	private Rigidbody2D rb;
+	private Vector3 startPosition;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		startPosition = transform.position; // Store the initial position of the character.
+	}
+
+	void Update()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector2(horizontal, vertical);
+		rb.velocity = movement * speed;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Obstacle"))
+		{
+			// Reset the character to the original position.
+			transform.position = startPosition;
+			rb.velocity = Vector2.zero; // Reset velocity to zero.
+		}
+	}
+}
---
+++ Assets/Assets_Lab06/Assets_Lab06/MoveFofura.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveFofura : MonoBehaviour
+{
+	public Vector3 moveSpeed;
+	public float spawnTime = 2f;
+	public float spawnDelay = 2f;
+
+	private Vector3 originalPosition;
+
+	// Use this for initialization
+	void Start()
+	{
+		moveSpeed = Vector3.left * Time.deltaTime;
+		originalPosition = transform.position; // Store the original position.
+		InvokeRepeating("ChangeSpeed", spawnDelay, spawnTime);
+	}
+
+	void ChangeSpeed()
+	{
+		moveSpeed = new Vector3(Random.Range(-1, -2), 0, 0) * 0.03f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position += moveSpeed;
+	}
+
+	// OnTriggerEnter2D is called when the GameObject enters a trigger collider.
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("ResetPosition"))
+		{
+			// Reset the GameObject to its original position.
+			transform.position = originalPosition;
+		}
+	}
+}
